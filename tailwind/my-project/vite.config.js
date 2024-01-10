90e2cf76f0a1b472dd6deb84e21df6357c16118a
marrdo <manuelmaldonado930329@gmail.com>
2024-01-10 20:23:05 +0100

    primera tailwind

---
+++ tailwind/my-project/vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  plugins: [],
+  server: {
+    host: true,
+    port: 90
+  },
+})
+
+// El puerto que puse es -p90:90 localhost/:90
